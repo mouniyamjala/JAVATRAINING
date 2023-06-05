d0a73ab6ccdbec38eb3f4b2a0015e6f5a63d8090
mouniyamjala <mouniyamjala95@gmail.com>
2023-06-05 18:03:44 -0400

    Employee java file

---
+++ Emp.java
@@ -0,0 +1,11 @@
+public class Emp {
+    public static void main(String args[])
+    {
+        public String empName;
+        public int empId;
+        public String empAddress;
+        public String empDeptName;
+        public String empMobileNumber;
+
+    }
+}
